f72fd62a5dd4a141db70eba1361610400bf19c17
Bill Phillips <wjphillips_git@outlook.com>
2015-12-03 22:31:16 -0800

    Fixed ability to pull characters from database

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,14 @@
 console.log("Routes have loaded succesfully");
-var characters = require('../controllers/characters.js');
+var characters_controller = require('../controllers/characters.js');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render("index");
 	}),
-	app.get('/character_list', function(req, res) {
-		res.render("characters");
+	app.get('/characters', function(req, res) {
+		characters_controller.return_all_characters(req, res);
+	}),
+	app.get('/characters/:id', function(req, res) {
+		characters.show(req, res);
 	})
 };
--- server/controllers/characters.js
+++ server/controllers/characters.js
@@ -1,18 +1,38 @@
-var mysql = require('mysql');
 var fs = require('fs');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost', 
 	user: 'root',
 	password: 'root',
 	database: 'smash_app'
 });
-connection.connect();
 
-module.exports = (function() {
-	return {
-		get_character_names: connection.query('SELECT name FROM characters', function(err, rows, fields) {
+// module.exports = (function() {
+// 	return {
+// 		get_all_character_names: connection.query('SELECT name FROM characters', function(err, rows) {
+// 			if (err) throw err;
+// 			return rows;
+// 		})
+// 	}
+// })();
+
+module.exports = {
+	return_all_characters: function(req, res) {
+		connection.query('SELECT * FROM characters', function(err, rows) {
 			if (err) throw err;
-			return rows;
+			var characters = rows;
+			res.render('characters', {characters: characters});
+		})
+	},
+	return_hello_world: function() {
+		var phrase = "hello world";
+		return(phrase);
+	},
+	show: function(req, res) {
+		connection.query('SELECT * FROM characters WHERE id = ?', req.params.id, function(err, row) {
+			if (err) throw err;
+			console.log(row);
+			return(row);
 		})
 	}
-})();
+};
