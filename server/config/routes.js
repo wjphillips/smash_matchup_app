63e862a05ce8f00c0d96ea5c8b8b683792d28435
Bill Phillips <wjphillips_git@outlook.com>
2015-12-09 23:41:57 -0800

    match up select now fully functional

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,11 +11,11 @@ module.exports = function(app) {
 	app.get("/characters/:id", function(req, res) {
 		characters_controller.show_base_character(req, res);
 	});
-	app.get('/matchups', function(req, res) {
-		res.redirect('/');
-	});
 	app.get("/matchups/:id", function(req, res) {
 		matchups_controller.show_matchup_by_id(req, res);
+	});
+	app.get("/new_matchup/:id1/:id2", function(req, res) {
+		matchups_controller.create(req, res);
 	});
 	app.post("/matchups", function(req, res) {
 		console.log("Matchup between " + req.body.base_char_name + " and " + req.body.matchup_char_name + " requested.");
--- server/controllers/matchups.js
+++ server/controllers/matchups.js
@@ -18,7 +18,7 @@ module.exports = {
 		});
 	},
 	show_matchup_by_chars: function(req, res) {
-		connection.query('SELECT * FROM match_ups m JOIN characters c1 on m.character1_id = c1.id JOIN characters c2 on m.character2_id = c2.id WHERE (character1_id = ? AND character2_id = ?) OR (character1_id = ? AND character2_id = ?)', [req.body.base_char_id, req.body.matchup_char_id, req.body.matchup_char_id, req.body.base_char_id], function(err, rows) {
+		connection.query('SELECT m.id as matchup_id, c1.id as char1_id, c1.name as char1_name, c1.image as char1_image, c2.id as char2_id, c2.name as char2_name, c2.image as char2_image FROM match_ups m JOIN characters c1 on m.character1_id = c1.id JOIN characters c2 on m.character2_id = c2.id WHERE (character1_id = ? AND character2_id = ?) OR (character1_id = ? AND character2_id = ?)', [req.body.base_char_id, req.body.matchup_char_id, req.body.matchup_char_id, req.body.base_char_id], function(err, rows) {
 			if(err) {
 				console.log("There was an error: " + err);
 				res.redirect('/characters/' + req.body.base_char_id);
@@ -26,12 +26,31 @@ module.exports = {
 			else if(rows.length != 0){
 				console.log("DB Returned: " + rows.length + " row(s).");
 				console.log("Data Returned: " + rows[0]);
-				res.set('title', 'test');
-				res.render('matchup', {matchup_data: rows, base_char_id: req.body.base_char_id});
+				res.render('matchup', {matchup_data: rows});
 			}
 			else{
-				console.log("No matchup was found.");
-				res.redirect('/characters/' + req.body.base_char_id);
+				console.log("No matchup was found. Attempting to create.");
+				res.redirect('/new_matchup/' + req.body.base_char_id + "/" + req.body.matchup_char_id);
+			};
+		});
+	},
+	create: function(req, res) {
+		console.log(req.params);
+		connection.query('INSERT INTO match_ups(character1_id, character2_id) VALUES (?, ?)', [req.params.id1, req.params.id2], function(err) {
+			if(err){
+				console.log("There was an error: " + err);
+				res.redirect('/');
+			}
+			else{
+				console.log("Matchup created successfully.");
+				connection.query('SELECT m.id as matchup_id, c1.id as char1_id, c1.name as char1_name, c1.image as char1_image, c2.id as char2_id, c2.name as char2_name, c2.image as char2_image FROM match_ups m JOIN characters c1 on m.character1_id = c1.id JOIN characters c2 on m.character2_id = c2.id ORDER BY m.id DESC LIMIT 1', function(err, rows) {
+					if(err){
+						console.log("There was an error: " + err);
+					}
+					else{
+						res.render('matchup', {matchup_data: rows});
+					}
+				});
 			};
 		});
 	}
