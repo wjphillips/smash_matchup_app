15705f04be1593ec5ee31a9ff92a3222847a6dfc
Bill Phillips <wjphillips_git@outlook.com>
2016-01-05 23:29:54 -0800

    Added comments to the match-up screen

--- server/controllers/matchups.js
+++ server/controllers/matchups.js
@@ -18,7 +18,7 @@ module.exports = {
 		});
 	},
 	show_matchup_by_chars: function(req, res) {
-		connection.query('SELECT m.id as matchup_id, c1.id as char1_id, c1.name as char1_name, c1.image as char1_image, c2.id as char2_id, c2.name as char2_name, c2.image as char2_image FROM match_ups m JOIN characters c1 on m.character1_id = c1.id JOIN characters c2 on m.character2_id = c2.id WHERE (character1_id = ? AND character2_id = ?) OR (character1_id = ? AND character2_id = ?)', [req.body.base_char_id, req.body.matchup_char_id, req.body.matchup_char_id, req.body.base_char_id], function(err, rows) {
+		connection.query('SELECT m.id as matchup_id, c1.id as char1_id, c1.name as char1_name, t.id as tip_id, t.content as tip_content, t.vote_count as vote_count, t.created_at as tip_created_date, t.character_id as tip_character_id, c1.image as char1_image, c2.id as char2_id, c2.name as char2_name, c2.image as char2_image, u.alias as tip_user_alias FROM match_ups m JOIN characters c1 on m.character1_id = c1.id JOIN characters c2 on m.character2_id = c2.id JOIN tips t on m.id = t.match_up_id JOIN users u on t.user_id = u.id WHERE (character1_id = ? AND character2_id = ?) OR (character1_id = ? AND character2_id = ?) ORDER BY tip_created_date DESC', [req.body.base_char_id, req.body.matchup_char_id, req.body.matchup_char_id, req.body.base_char_id], function(err, rows) {
 			if(err) {
 				console.log("There was an error: " + err);
 				res.redirect('/characters/' + req.body.base_char_id);
